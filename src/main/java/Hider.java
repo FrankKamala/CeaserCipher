24049f75804447cf7f000bd9577eeac34f431590
FrankKamala <frankkamala@gmail.com>
2019-12-15 22:40:00 +0300

    first test and logic

--- src/main/java/Hider.java
+++ src/main/java/Hider.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Hider {
+ private String mData;
+ private Scanner we= new Scanner(System.in);
+public boolean isString(){
+    System.out.println("Enter Input :");
+    System.out.println("");
+    if(we.hasNextInt()){
 
-
+    }
+    return false;
+}
 }
--- src/test/java/HiderTest.java
+++ src/test/java/HiderTest.java
@@ -1,6 +1,14 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HiderTest {
     Hider encrp=new Hider();
+
+    @Test
+    public void doTest_On_StringValue(){
+        assertEquals(true,encrp.isString());
+
+    }
 
 }
