e82a2dbebb5b55e63de42a8b8e25de7aadb5662a
FrankKamala <frankkamala@gmail.com>
2019-12-16 09:56:51 +0300

    comment Tests

--- src/test/java/HiderTest.java
+++ src/test/java/HiderTest.java
@@ -5,16 +5,17 @@ import static org.junit.Assert.*;
 public class HiderTest {
     Hider encrp=new Hider();
 
-    @Test
+    @Test //1st test case
     public void doTest_On_StringValue(){
         assertEquals(true,encrp.isString());
 
     }
- @Test
+ @Test //.2ndtest case on Encryption or encoding
     public void doTest_On_Encoding(){
     assertEquals("bc",encrp.encoderData("HI",20));
     }
-    @Test
+
+    @Test // 3rd test on Decode data
     public  void doTest_On_Decode(){
         assertEquals("hi",encrp.decoderData("bc",20));
     }
